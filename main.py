4667d92f6731b203c7ee290a34971eed7b53681d
Vi <69354448+villayer@users.noreply.github.com>
2021-08-11 17:11:03 +0000

    stuff

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
+import os
 import discord
 
+os.environ['TOKEN']
+
+client = discord.Client()
+
+
+# registering an event
+@client.event
+async def on_ready():
+    print('we are live as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$Vi_'):
+        await message.channel.send('Praise Vi, All hail /BPG/')
+
+
+client.run(os.getenv('TOKEN'))
