22a5173e96c1c0d697b78b8c9e2030f64433a42e
Vi <69354448+villayer@users.noreply.github.com>
2021-08-31 11:48:37 +0000

    added help, dice and calc triggers

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
 import discord
+import random
+from discord.ext import commands
 
+bot = commands.Bot(command_prefix='!')
 os.environ['TOKEN']
-
 client = discord.Client()
 
 
@@ -12,13 +14,21 @@ async def on_ready():
     print('we are live as {0.user}'.format(client))
 
 
+@bot.command()
+async def calc(ctx, arg):
+    await ctx.send(arg)
+
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
+    if message.content.startswith('!help'):
+        await message.channel.send('`commands:\n!help: show this dialoge\n!dice: role a dice\n!calc: calculates your stuff`')
 
-    if message.content.startswith('$Vi_'):
-        await message.channel.send('Praise Vi, All hail /BPG/')
+    if message.content.startswith('!dice'):
+        rand_num = random.randint(1, 6)
+        await message.channel.send('here is your lucky number: ' + str(rand_num) )
 
 
 client.run(os.getenv('TOKEN'))
